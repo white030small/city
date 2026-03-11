27c0e10d3cc28f88dd504c6b165936dda3689336
white <leo070818@gmail.com>
2026-03-12 01:30:29 +0800

    同步兩邊的動作

--- Assets/script/charater/mainchar.cs
+++ Assets/script/charater/mainchar.cs
@@ -6,14 +6,22 @@ public class mainchar : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce = 6f; // 跳躍的力量
     public float runMultiplier = 1.5f;
+    private float moveInputX;
+    private bool isRunning ;
 
     [Header("同步目標")]
     public Rigidbody2D rbReality;   // 現實層的剛體
     public Rigidbody2D rbSpirit;    // 靈界層的剛體
 
-    private float moveInputX;
+    [Header("世界")]
     public int world = 0 ;//現實:0，靈界:1
-    private bool isRunning ;
+
+    [Header("跳躍上限")]
+    public int maxjump = 1;
+    public int jumpCount = 0;
+    
+    [Header("分界線")]
+    public float dividerY = 0f;  // 白線的 Y 座標
 
     void Update()
     {
@@ -21,28 +29,44 @@ public class mainchar : MonoBehaviour
         moveInputX = Input.GetAxisRaw("Horizontal");
         isRunning = Input.GetKey(KeyCode.LeftShift);
 
+        // 地面偵測
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position, 
+            groundCheckRadius, 
+            groundLayer
+        );
+
+        // 落地時重置跳躍次數
+        if (isGrounded)
+        {
+            jumpCount = 0;
+        }
+
         // 2. 偵測跳躍按鍵 (按下空白鍵)
         if (Input.GetKeyDown(KeyCode.Space))
         {
             PerformJump();
         }
 
+        //切換世界
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             changeworld();
         }
         
+
         if(world == 1)
         {
-            ApplyHorizontalMovement(rbSpirit);
+            ApplyHorizontalMovement(rbSpirit);//靈魂單獨行動
         }
         else
         {
-            ApplyHorizontalMovement(rbSpirit);
             ApplyHorizontalMovement(rbReality);
+            spritgoreality();//同步行動
         }
         
     }
+    
     void changeworld()
     {
         if(world == 0)
@@ -62,8 +86,21 @@ public class mainchar : MonoBehaviour
         ApplyHorizontalMovement(rbSpirit);
     }*/
 
+    void spritgoreality()
+    {
+        // 鏡像 Y：如果現實在線上方 +1，靈體就在線下方 -1
+        float mirroredY = 2 * dividerY - rbReality.position.y;
+        
+        rbSpirit.position = new Vector2(rbReality.position.x, mirroredY);
+        rbSpirit.linearVelocity = new Vector2(rbReality.linearVelocity.x, -rbReality.linearVelocity.y);
+    }
+
     void PerformJump()
     {
+        if(jumpCount >= maxjump)return;
+
+        jumpCount++;
+
         // 現實層：往上跳 (+Y)
         if (rbReality != null && world == 0)
         {
